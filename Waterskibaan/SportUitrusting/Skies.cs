b805f6e79ac2d8f8c62501e64429b4aa4b1ed8e0
Sake Elfring <43179986+saakee@users.noreply.github.com>
2018-09-14 11:29:43 +0200

    Opdracht 2 (#3)

---
+++ Waterskibaan/SportUitrusting/Skies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterSkiBaan.SportUitrusting
+{
+    public class Skies : SportArtikel
+    {
+        public Skies(int id) : base(id) {}
+    }
+}
