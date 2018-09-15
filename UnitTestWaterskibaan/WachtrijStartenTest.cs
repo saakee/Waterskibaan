babf018eb099ab463336f77827b80433adcfad56
DESKTOP-P03HJ5A\rvanb <rvanbostelen@outlook.com>
2018-09-15 18:32:22 +0200

    Unittest probeersols #weProberenMaarWatToch.

--- UnitTestWaterskibaan/WachtrijStartenTest.cs
+++ UnitTestWaterskibaan/WachtrijStartenTest.cs
@@ -1,14 +1,46 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WaterSkiBaan.Wachtrijen;
+using WaterSkiBaan.Medewerkers;
+using WaterSkiBaan.Sporters;
 
 namespace UnitTestWaterskibaan
 {
     [TestClass]
     public class WachtrijStartenTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void Test_InitializingWachtrijStarten_ShouldNotBeEqualToNull()
         {
+            WachtrijStarten wachtrijStarten = new WachtrijStarten();
+            Assert.IsNotNull(wachtrijStarten);
         }
+        [TestMethod]
+        public void Test_AddingSporterToQueue_ShouldSucced()
+        {
+            WachtrijStarten wachtrijStarten = new WachtrijStarten();
+            Wakeboarder wakeboarder = new Wakeboarder();
+            
+            wachtrijStarten.voegSporterToeAanRij(wakeboarder);
+        }
+
+
+        [TestMethod]
+        public void Test_AddingMedewerkerToQueue_ShouldReturnException()
+        {
+            WachtrijStarten wachtrijStarten = new WachtrijStarten();
+            Instructeur medewerker = new Instructeur();
+
+            try
+            {
+                wachtrijStarten.voegSporterToeAanRij(medewerker);
+                Assert.Fail("Exception throw FAIL!");
+            }
+            catch (ArgumentException)
+            {
+                // pass
+            }
+        }
+
     }
 }
